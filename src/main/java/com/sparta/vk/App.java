daf6f16ae2ddf86f18a6a645dcd0686c39bd9924
V Kapoor <vkapoor@spartaglobal.com>
2019-04-17 14:31:16 +0100

    Initial commit of project structure

---
+++ src/main/java/com/sparta/vk/App.java
@@ -0,0 +1,17 @@
+package com.sparta.vk;
+
+import org.json.simple.JSONObject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        JSONObject userDetails = new JSONObject();
+
+        System.out.println(userDetails.getClass());
+    }
+}
